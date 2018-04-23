df44244b52529e5985f6050a3c1d00cb2bc40c0b
Mathieu CAROFF <mathieu.caroff@etu.emse.fr>
2018-04-23 15:55:39 +0200

    Suppression des fichiers inutiles

--- Traqumoto/sample/mv-config.lua/mv-config.lua
+++
@@ -1,6 +0,0 @@
-local config = {}
-
--- Copier-coller ici les samples choisi pour chaque mv.*.config.lua
--- Copy-paste here the chosen sample for each mv.*.config.lua
-
-return config
---
+++ src/ihm/quit.py
@@ -0,0 +1,5 @@
+"""
+Gère la detection de la fermeture de la fenêtre principale
+ainsi que l'action clavier de quitter la lecture.
+"""
+

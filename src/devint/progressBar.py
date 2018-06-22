064a7e41d8ca7479f81bc5665cdb9409d982a22d
Mathieu CAROFF <mathieu.caroff@etu.emse.fr>
2018-06-22 22:01:36 +0200

    Fix wrong progressBar width

--- src/devint/progressBar.py
+++ src/devint/progressBar.py
@@ -19,13 +19,14 @@ def setupClickHook(mouseCallbackList, bufferShape, sensitivityHeight, jumpToFram
     
     mouseCallbackList.append(evListener)
 
-def drawBar(progressBarConfig, width, buffer, advancementPercentage):
+def drawBar(progressBarConfig, buffer, advancementPercentage):
     """
     Dessine une barre horizontale en bas à gauche sur advancementPercentage de l'écran.
     La barre fait progressBarConfig.height pixels de haut et est de couleur progressBarConfig.fgColor (BGR) à gauche,
     et progressBarConfig.bgColor (BGR) à droite.
     """
     height = progressBarConfig.height
+    width = buffer.shape[1]
     advancementPx = int(advancementPercentage * width)
     buffer[-height:, :advancementPx, :] = progressBarConfig.fgColor
     buffer[-height:, advancementPx:, :] = progressBarConfig.bgColor
